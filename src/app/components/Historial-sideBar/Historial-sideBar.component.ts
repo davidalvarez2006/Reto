3d2a954258a0b336b1827628f2437d57f3e06add
Sendoa6 <sendoa.perez06@somo.eus>
2025-03-28 11:07:34 +0100

    Historial

---
+++ src/app/components/Historial-sideBar/Historial-sideBar.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-side-bar',
+  imports: [],
+  templateUrl: './Historial-sideBar.component.html',
+  styleUrl: './Historial-sideBar.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HistorialSideBarComponent { }
