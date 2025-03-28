93c920e94ae1cc226f139c4381d387662df7c936
davidalvarez2006 <david.alvarez06@somo.eus>
2025-03-28 12:40:21 +0100

    Texto

--- src/app/pages/principal-page/principal-page.component.ts
+++ src/app/pages/principal-page/principal-page.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ChatHistorialComponent } from "../../components/chat-historial/chat-historial.component";
 import { ChatTextbarComponent } from "../../components/chat-textbar/chat-textbar.component";
-import { HistorialSideBarComponent } from "../../components/Historial-sideBar/Historial-sideBar.component";
 
 @Component({
   selector: 'app-principal-page',
-  imports: [ChatHistorialComponent, ChatTextbarComponent, HistorialSideBarComponent],
+  imports: [ChatHistorialComponent, ChatTextbarComponent],
   templateUrl: './principal-page.component.html',
   styleUrl: './principal-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
