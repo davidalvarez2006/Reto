37243ff8100da13e55f69b190d9a332ce5dacc90
davidalvarez2006 <david.alvarez06@somo.eus>
2025-03-31 08:29:46 +0200

    n

--- src/app/components/chat-historial/chat-historial.component.ts
+++ src/app/components/chat-historial/chat-historial.component.ts
@@ -1,42 +1,10 @@
-import { Component } from "@angular/core";
-import { ChatService } from "../../services/chat-service.service";
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat-historial',
+  imports: [],
   templateUrl: './chat-historial.component.html',
   styleUrl: './chat-historial.component.css',
-  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChatHistorialComponent {
-  messages: any[] = [];
-  newMessage: string = '';
-
-  constructor(private chatService: ChatService) {}
-
-  sendMessage() {
-    if (this.newMessage.trim() === '') return;
-
-    this.chatService.sendMessage(this.newMessage).subscribe(response => {
-      this.messages.push({ text: this.newMessage, sender: 'user' });
-      this.newMessage = '';
-    });
-  }
-
-  loadMessages() {
-    this.chatService.getMessages().subscribe(response => {
-      this.messages = response;
-    });
-  }
-
-  ngOnInit() {
-    this.loadMessages(); // Carga los mensajes al iniciar
-  }
-
-  trackByIndex(index: number, item: any): number {
-    return index;
-  }
-}
-
-
-
+export class ChatHistorialComponent { }
