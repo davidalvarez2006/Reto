e49c9636ee17974c224f1c566fca67e9e38a9649
davidalvarez2006 <david.alvarez06@somo.eus>
2025-04-01 09:06:27 +0200

    parse

--- src/app/components/chat-historial/chat-historial.component.ts
+++ src/app/components/chat-historial/chat-historial.component.ts
@@ -7,30 +7,7 @@ import { MessageService } from '../../services/chat-service.service';// Asegúra
   styleUrls: ['./chat-historial.component.css'],
 })
 export class ChatHistorialComponent {
-  // mensajes = signal<{ texto: string; tipo: 'usuario' | 'bot' }[]>([]);
-
-  // constructor(private messageService: MessageService) {}
-
-  // enviarMensaje(texto: string) {
-  //   if (texto.trim() === '') return;
-
-  //   // Agregar mensaje del usuario
-  //   this.mensajes.update((msgs) => [...msgs, { texto, tipo: 'usuario' }]);
-
-  //   this.messageService.llamada(texto).subscribe({
-  //     next: (respuesta) => {
-  //       // Convertir respuesta a string si es un objeto
-  //       const textoRespuesta = typeof respuesta === 'string' ? respuesta : JSON.stringify(respuesta);
-
-  //       // Agregar respuesta del bot
-  //       this.mensajes.update((msgs) => [...msgs, { texto: textoRespuesta, tipo: 'bot' }]);
-  //     },
-  //     error: (error) => console.error('Error al enviar mensaje:', error)
-  //   });
-  // }
-  // @Input() mensajes: mensajes[] = [];
   mensajes = input<mensajes[]>()
-
 }
 
 interface mensajes{
